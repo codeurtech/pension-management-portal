ef13bdaf6120edf20de67750ae59d2ec73051a53
U R <upkarking1@gmail.com>
2022-08-22 11:59:26 +0530

    minor additions

--- backend/api-gateway/src/main/java/com/pension/management/apigateway/filter/LoggingFilter.java
+++ backend/api-gateway/src/main/java/com/pension/management/apigateway/filter/LoggingFilter.java
@@ -7,9 +7,11 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class LoggingFilter implements GlobalFilter {
 
 	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
